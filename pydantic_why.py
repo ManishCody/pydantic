869b0de15c1bf0ba0ed89d820e7e751eca6976e5
ManishCody <algowhiz@gmail.com>
2025-09-14 09:29:40 +0530

    field validator

--- pydantic_why.py
+++ pydantic_why.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel
-from typing import List , Dict
+from pydantic import BaseModel , EmailStr , Field
+from typing import List  , Optional
 
+class contactInfo(BaseModel):
+    email:EmailStr
+    phone:str
 
 class Patient(BaseModel):
-    name:str    
-    age:int
+    name:str   = Field(min_length=3 , max_length=50) 
+    age:int =  Field(ge=18, lt=100)
     weight:float
-    married:bool
-    allergies:List[str]
-    contact_deatils:Dict[str,str]    
+    married:Optional[bool] = None
+    allergies:List[str]  # List is used for validating inner values also 
+    contact_deatils: contactInfo   
 
 def insert_patient_data(patient:Patient):
     print(patient.name)
@@ -19,7 +22,7 @@ def insert_patient_data(patient:Patient):
     print(patient.contact_deatils)
     print("Data inserted successfully")
 
-patient_info = { "name": "John Doe", "age": '20' , "weight": 70.5, "married": False, "allergies": ["pollen", "nuts"], "contact_deatils": {"email": "jhon@gmail.com"} }
+patient_info = { "name": "John Doe", "age": '20' , "weight": 70.5, "allergies": ["pollen", "nuts"], "contact_deatils": {"email": "jhon@gmail.com" , "phone":"9083729202"} }
 
 patient1 = Patient(**patient_info)
 
